cfc6337802084705454c066ce31cefa09e8fb801
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-01-09 12:46:33 +0530

    This is a number system program

--- NumberSystem.java
+++ NumberSystem.java
@@ -7,3 +7,14 @@ class Number{
     }
 }
 //git num date- 08.01.26
+
+
+// for reverse of number
+class reverse{
+    public static void main(String[]args){
+        int n;
+        for(n=5;n>=1;n--){
+            System.out.println(n);
+        }
+    }
+}
