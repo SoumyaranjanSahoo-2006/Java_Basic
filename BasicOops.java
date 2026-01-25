22ae5ebb1a9766a064cb23550e406039353b954a
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-01-25 20:37:56 +0530

    just update

--- BasicOops.java
+++ BasicOops.java
@@ -343,7 +343,7 @@ class Student1Main{
 // Create Shape class with method draw()
 // Override it in Circle class.
 
-//  LEVEL 6: Abstraction
+// 🔰 LEVEL 6: Abstraction
 
 // Create an abstract class Bank with abstract method getInterestRate()
 // Implement it in SBI class.
