d620be01b652159201dd2bb46c915ca691eba61c
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-01-27 12:13:34 +0530

    added class odd

--- NumberSystem.java
+++ NumberSystem.java
@@ -58,3 +58,18 @@ class CountDigitofEachNumber {
 		System.out.println(count);
 	}
 }
+
+
+
+//To find the sumof the each digit
+class SumofEachDigit {
+	public static void main(String[] args) {
+		int num;
+		int sum=0;
+		for( num=12345;num>0;num=num/10) {
+			int rem=num%10;
+			sum=sum+rem;
+		}
+		System.out.println(sum);
+	}
+}
