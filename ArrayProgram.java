55c1ba0ccb656fd5d9b7772ad998deecb1a38223
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-01-29 08:25:35 +0530

    Array program

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -1,4 +1,7 @@
-//! Write a java program to find sum of all element from an array or by using variable argument method.
+import java.util.Arrays;
+
+
+//! 1. Write a java program to find sum of all element from an array or by using variable argument method.
 
 class SumOfAllElement{
     public static void main(String[]args){
@@ -14,3 +17,28 @@ class SumOfAllElement{
         return sum;
     }
 }
+
+
+//! 2. WAJP to find sum of all even number and multiplication of al odd  number in a array.
+
+class AllEvenOdd{
+    public static void main(String[]args){
+        m1(1,2,3,4,5,6,7,8,9);
+    }
+
+    static int m1(int...a){
+        int sum=0;
+        int multiplay=1;
+        for(int temp:a){
+            if(temp%2==0){
+                sum+=temp;
+            }
+            else{
+                multiplay*=temp;
+            }
+        }
+        System.out.println("sum "+sum+", multiplication "+multiplay);
+        return sum;
+    }
+}
+
