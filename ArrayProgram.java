e3e283f988bca774c9fcef8f2d80a21d7b7e9f43
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-01-29 12:30:36 +0530

    Array program

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -66,3 +66,79 @@ class UpdateElement{
         a[index]=newElement;
     }
 }
+
+//! WAJP toupdate value off an array with new gven value. i/p- {3,2,1,4,3,5,4}, old value=3,new value=10 .
+
+class UpdateValue{
+    public static void main(String[]args){
+        int a[]={3,2,1,4,3,5,4};
+        System.out.println(Arrays.toString(a));
+        m1(a,10,3);
+        System.out.println(Arrays.toString(a));
+    }
+    static void m1(int a[],int newValue,int oldValue){
+        for(int i=0;i<a.length;i++){
+            if(a[i]==oldValue){
+                a[i]=newValue;
+            }
+        }
+    }
+}
+
+
+//! WAJP to arrange the element in sequence. i/p={1,5,3,7,4,6,10,9},o/p={1,3,4,5,6,7,9,10}
+
+class ArrangeElement{
+    public static void main(String[]args){
+        int a[]={1,5,3,7,4,6,10,9};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
+
+
+// class ArrangeElement2{
+//    public static void main(String[]args){
+//         int a[]={1,5,3,7,4,6,10,9};
+//         arrange(a);
+       
+//     }
+//     static void arrange(int a[]){
+//         Arrays.sort(a);
+//         for(int i=0;i<a.length;i++){
+//             if(a[i]-1!=a[i-1]){
+//                 System.out.println(a[i]-1);
+//             }
+//         }
+//     } 
+// }
+
+
+//! WAJP to insert an element at last of an array.
+
+class InsertValue{
+    public static void main(String[]args){
+        int a[]={8,5,4,9,6,2,7};
+        int b[]=new int[a.length+1];
+        for(int i=0;i<a.length;i++){
+            b[i]=a[i];
+        }
+        b[a.length]=1;
+        a=b;
+        System.out.println(Arrays.toString(a));
+    }
+
+}
+
+
+//! WAJP to find the all even number of an array.
+class ArrayEven{
+    public static void main(String[]args){
+        int a[]={1,2,3,4,5,6,7,8,9};
+        for(int temp:a){
+            if(temp%2==0){
+                System.out.println(temp);
+            }
+        }
+    }
+}
