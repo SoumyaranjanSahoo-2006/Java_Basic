198d298ebadb1a4b5547a7731ece02e2cb1d60c6
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-02-05 21:03:55 +0530

    updated

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -142,3 +142,25 @@ class ArrayEven{
         }
     }
 }
+
+//! WAJP to print all the prime number of an array.
+
+class ArrayPrime{
+    public static void main(String[]args){
+        int a[]= {1,2,3,4,5,6,7,8,9,10};
+        for(int temp:a){
+            if(isPrime(temp)){
+                System.out.println(temp);
+            }
+        }
+    }
+    static boolean isPrime(int n){
+        if(n<=1) return false
+        int count=0;
+        for (int i=1;i<=n;i++){
+            if(n%i==0)
+            count++;
+        }
+        return count==2;
+    }
+}
