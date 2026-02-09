d0cc948c4e8be2077ea66e5b5004a3fee902f7b9
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-02-09 19:29:36 +0530

    aded SumOfCharDigit class

--- StringPrograms.java
+++ StringPrograms.java
@@ -61,3 +61,19 @@ class Palindrome1{
         return s.equals(reverse);
     }
 }
+
+//! WAJP to find sum of all digits inside a string.
+
+class SumOfCharDigit{
+    public static void main(String[]args){
+        String s="1su23bhka41ti";
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='0'&& ch<='9'){
+                sum=sum+(ch-'0');
+            }
+        }
+        System.out.println(sum);
+    }
+}
