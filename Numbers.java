0b4669a15ad9c858a82ed899a541817d52df036c
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-10 21:38:07 +0530

    add the Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -114,3 +114,23 @@ class SumOfDivisor {
         sc.close();
     }
 }
+
+
+
+/* Write a Java program to print the following number pattern using nested loops:
+
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5       */
+class NumberPattern{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++) {
+                System.out.print(" " + i);
+            }
+            System.out.println();
+        }
+    }
+}
