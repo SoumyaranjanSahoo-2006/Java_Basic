71e87f85083b4de3b3dc2e2c3106a4e84dc8f328
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-10 21:26:09 +0530

    add the Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -21,3 +21,25 @@ class PrintDivisor {
     }
 }
 
+/*
+Write a Java program to print all divisors of a number with improved time complexity.
+
+Example:
+36 → 1, 2, 3, 4, 6, 9, 12, 18, 36
+28 → 1, 2, 4, 7, 14, 28     */
+
+class PrintDivisor2 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n/2; i++) {
+            if(n % i == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println(n);
+    }
+}
+
