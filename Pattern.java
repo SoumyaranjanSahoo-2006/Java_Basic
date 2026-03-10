0a9c46742ca195e94dc9d2eca553462dfc4e4ac0
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-10 21:00:51 +0530

    add the pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
+
+
+
 // Write a Java program to print a horizontal pattern of 5 stars using a loop.
 class Pattern{
     public static void main(String[]args){
@@ -84,12 +89,55 @@ class PrincipalDiagonal {
 **  *
 *****        */
 
-
-public class AntiDiagonal {
+class AntiDiagonal {
     public static void main(String[] args) {
         for(int i = 1; i <= 5; i++) {
             for(int j = 1; j <= 5; j++) {
                 if(i == 1 || i == 5 || j == 1 || j == 5 || i + j == 6) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+/* Write a Java program that takes the number of rows as input from the user and prints a square pattern with both diagonals and borders.
+
+Example Input:
+
+
+Enter number of row-
+9
+
+
+Output:
+
+*********
+**     **
+* *   * *
+*  * *  *
+*   *   *
+*  * *  *
+* *   * *
+**     **
+*********  */
+
+
+
+class DigonalScanner {
+    public static void main(String[] args) {
+        System.out.println("Enter number of row- ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1 || i == n || j == 1 || j == n || i + j == n + 1 || i == j) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
