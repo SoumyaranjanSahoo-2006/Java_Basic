d58ab3152104f703cf455af3b8336f4635becf30
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-10 21:28:50 +0530

    add the Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -43,3 +43,35 @@ class PrintDivisor2 {
     }
 }
 
+
+/* Write a Java program to print all divisors of a number using an optimized approach (up to √n).
+Example Output:
+Enter a number:
+36
+1
+36
+2
+18
+3
+12
+4
+9
+6       */
+
+class DivisorNumber_2 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                System.out.println(i);
+                if(n / i != i) {
+                    System.out.println(n / i);
+                }
+            }
+        }
+    }
+}
+
