377e7e0c027af65c3600b4e6dea64a65a62bf528
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-10 21:31:17 +0530

    add the Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -75,3 +75,42 @@ class DivisorNumber_2 {
     }
 }
 
+
+/* Write a Java program to print all divisors of a number and calculate the sum of all divisors using an optimized approach (up to √n).
+Example Output:
+Enter a number:
+36
+1
+36
+2
+18
+3
+12
+4
+9
+6
+sum= 91     */
+
+class SumOfDivisor {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                sum += i;
+                System.out.println(i);
+
+                if(n / i != i) {
+                    System.out.println(n / i);
+                    sum = sum + (n / i);
+                }
+            }
+        }
+
+        System.out.println("sum= " + sum);
+        sc.close();
+    }
+}
