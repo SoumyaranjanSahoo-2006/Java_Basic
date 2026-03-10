ff369d8e715ee0d63ed481e784091da3b4c3854a
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-10 20:36:36 +0530

    add the pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,7 +1,28 @@
+// Write a Java program to print a horizontal pattern of 5 stars using a loop.
 class Pattern{
     public static void main(String[]args){
         for(int i=1;i<=5;i++){
             System.out.print(" *");
         }
     }
+}
+
+
+/* Write a Java program to print the following pattern using nested loops:
+
+*****
+*****
+*****
+*****
+*****    */
+class pattern2{
+    public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+		    System.out.println();
+		}
+	}
+
 }
