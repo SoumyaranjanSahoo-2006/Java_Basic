ab041853041765f32d0cf00e8eb5ad9376c1f887
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-10 20:46:14 +0530

    add the pattern.java

--- Pattern.java
+++ Pattern.java
@@ -50,3 +50,27 @@ class HollowSquare{
 	}
 
 }
+
+/* Write a Java program to print the following pattern (square with principal diagonal) using nested loops:
+
+*****
+**  *
+* * *
+*  **
+*****    */
+
+class PrincipalDiagonal {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++) {
+                if(i == 1 || i == 5 || j == 1 || j == 5 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
