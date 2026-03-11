71f114d3ad65048f62b686c17dfc0c674fe89007
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-11 21:00:16 +0530

    add the Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -160,3 +160,30 @@ class NumberPattern2{
     }
 }
 
+
+/* Write a Java program that takes a number as input and prints the following pattern:
+
+1	2	3	4	5
+6	7	8	9	10
+11	12	13	14	15
+16	17	18	19	20
+21	22	23	24	25      */
+
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int val = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.print(val + "\t");
+                val++;
+            }
+            System.out.println();
+        }
+    }
+}
+
