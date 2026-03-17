88fd5324faf99f91603bdc0c75981a3fed2688fd
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-03-17 20:54:48 +0530

    adding a star program

--- Pattern.java
+++ Pattern.java
@@ -148,3 +148,29 @@ class DigonalScanner {
     }
 }
 
+
+
+
+/* Output
+*****
+****
+***
+**
+*            */
+
+
+class DemoPattern {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number of row: ");
+        int n = new Scanner(System.in).nextInt();
+
+        for(int i = n; i >= 1; i--) {
+            for(int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
